d422dc487b2a4f56c460e1c3c5186901696f247f
jbhampton1 <126513556+JoBHampton@users.noreply.github.com>
2025-11-09 17:48:44 -0600

    changes

--- backend/newEntryPoint.js
+++ backend/newEntryPoint.js
@@ -188,7 +188,7 @@ app.delete("/favorites", async (req, res) => {
 const CRUD = require('./CRUD');
 app.use('/items', CRUD);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// module.exports = app;
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
 
